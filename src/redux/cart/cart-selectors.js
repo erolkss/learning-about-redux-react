b3b634df6007f751656e07e31f160fddee0e941d
Lucas Lima <lucaserolima@gmail.com>
2024-05-16 22:28:41 -0400

    feat: Learning about redux 'Selectors' to calculate the price of the quantity of products added to the cart

--- src/redux/cart/cart-selectors.js
+++ src/redux/cart/cart-selectors.js
@@ -6,3 +6,11 @@ export const selectProductsCount = (rootReducer) => {
     0
   );
 };
+
+export const selectProductsTotalPrice = (rootReducer) => {
+  return rootReducer.cartReducer.products.reduce(
+    (acc, curr) => acc + curr.price * curr.quantity,
+    0
+  );
+};
+
