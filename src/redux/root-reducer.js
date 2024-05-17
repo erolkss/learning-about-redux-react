31a440066926d60cdd78ff2f39af04eb2e378951
Lucas Lima <lucaserolima@gmail.com>
2024-05-16 21:16:27 -0400

    feat: Adding Product to Cart with useDispatch()

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
 import userReducer from "./user/reducer";
+import cartReducer from "./cart/reducer";
 
-export const rootReducer = combineReducers({ userReducer });
-
+export const rootReducer = combineReducers({ userReducer, cartReducer });
