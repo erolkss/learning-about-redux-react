f5b1eb2ae91beed5d247f3c4018884c51b0c5193
Lucas Lima <lucaserolima@gmail.com>
2024-05-16 20:24:55 -0400

    feat: UserReducer created

--- src/redux/user/reducer.js
+++ src/redux/user/reducer.js
@@ -1,4 +1,11 @@
-
-const userReducer = () => {};
+const initialState = {
+  currentUser: null,
+};
+const userReducer = (state = initialState, action) => {
+  if (action.type === "user/login") {
+    return {...state, currentUser: 10 };
+  }
+  return state;
+};
 
 export default userReducer;
