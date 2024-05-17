e96255fcd31f99f0b18d06bb901acdd826860c96
Lucas Lima <lucaserolima@gmail.com>
2024-05-16 21:39:28 -0400

    feat: Accessing the CartReducer in the Header component, using it to count items within the cart

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 // Components
 import Cart from "../cart/index";
@@ -12,12 +12,17 @@ function Header() {
   const [cartIsVisible, setCartIsVisible] = useState(false);
 
   const { currentUser } = useSelector((rootReducer) => rootReducer.userReducer);
+  const { products } = useSelector((rootReducer) => rootReducer.cartReducer);
 
   const dispatch = useDispatch();
   console.log({ currentUser });
   const handleCartClick = () => {
     setCartIsVisible(true);
   };
+
+  const productsCount = useMemo(() => {
+    return products.reduce((acc, curr) => acc + curr.quantity, 0);
+  }, [products])
 
   const handleLoginClick = () => {
     dispatch(loginUser({name: "Lucas", email: "lucas@gmail.com"}));
@@ -38,7 +43,7 @@ function Header() {
           <div onClick={handleLoginClick}>Login</div>
         )}
 
-        <div onClick={handleCartClick}>Carrinho</div>
+        <div onClick={handleCartClick}>Carrinho ({productsCount})</div>
       </Styles.Buttons>
 
       <Cart isVisible={cartIsVisible} setIsVisible={setCartIsVisible} />
